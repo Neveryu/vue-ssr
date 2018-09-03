37e11d93e4242b5dc381797f1572901e26af3b85
Administrators <Administrators>
2018-09-03 18:48:57 +0800

    first commit

---
+++ build/webpack.config.base.js
@@ -0,0 +1,42 @@
+const path = require('path')
+
+const config = {
+  target: 'web',
+  // 加个默认的 entry 也没有关系，后面可以覆盖
+  entry: path.join(__dirname, '../src/index.js'),
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.join(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: 'resources/[path][name].[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config
