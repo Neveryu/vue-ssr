eb77d57e079476619c062e41a5b21c8d6ace6cf8
yudong <never_yu@foxmail.com>
2018-10-30 23:00:45 +0800

    20181030 23:00

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -4,6 +4,7 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const ExtractPlugin = require('extract-text-webpack-plugin')
 const baseConfig = require('./webpack.config.base')
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -13,7 +14,8 @@ const defaultPlugins = [
       NODE_ENV: isDev ? '"development"' : '"production"'
     }
   }),
-  new HTMLPlugin()
+  new HTMLPlugin(),
+  new VueSSRClientPlugin()
 ]
 
 const devServer = {
--- server/routers/dev-ssr.js
+++ server/routers/dev-ssr.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 const MemoryFS = require('memory-fs')
 const webpack = require('webpack')
 const path = require('path')
+const fs = require('fs')
 const VueServerRenderer = require('vue-server-renderer')
 
 const serverConfig = require('../../build/webpack.config.server')
@@ -25,4 +26,22 @@ serverCompiler.watch({}, (err, stats) => {
   })
 
   const bundlePath = path.join(serverConfig.output.path, 'vue-ssr-server-bundle.json')
+
+  bundle = JSON.parse(mfs.readFileSync(bundlePath, 'utf-8'))
 })
+
+const handleSSR = async (ctx) => {
+  if(!bundle) {
+    ctx.body = '你等一会，别着急...'
+    return
+  }
+
+  const clientManifestResp = await axios.get('http://127.0.0.1:8000/vue-ssr-client-manifest.json')
+
+  const template = fs.readFileSync(path.join(__dirname, '../server.template.ejs'))
+  const renderer = VueServerRenderer.createBundleRenderer(bundle, {
+    runInNewContext: false,
+    template
+  })
+}
+
